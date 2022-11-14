f179c460b37d35826606d007213625e9cbfb8d3b
iw2872 <iw2872@naver.com>
2022-11-14 19:26:52 +0900

    2022-11-14

--- PAM.py
+++ PAM.py
@@ -1,22 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from pylab import *
+from scipy import signal
 
-percent=50.0
-TimePeriod=10.0 #Frozen Value Do not change
-Cycles=10 #Frozen Value Do not change
-dt=0.01 #Frozen Value Do not change
+def sin_wave(amp, freq, time):
+    return amp * np.sin(2*np.pi*freq*time)
+def pulse_wave(amp, freq, sample):
+    #return np.arange(sample) % period < width
+    p = amp * signal.square(2 * np.pi * freq * time)
+    for i in range(len(p)):
+        if p[i] < 0:
+            p[i] = 0
+    return p
 
-t=np.arange(0,Cycles*TimePeriod,dt);
-pwm= t%TimePeriod<TimePeriod*percent/100
+if __name__ == '__main__':
+    #time = np.arange(0, 0.1, 0.0001)
+    time = np.linspace(0, 0.002, 5000)
+    sin1 = sin_wave(1, 10000, time)
+    pulse = pulse_wave(1, 1000, time)
 
-x=np.linspace(-10,10,10000) #Frozen Value Do not change
-y=(np.sin(x))
+    mod = sin1 + pulse
 
-y[(pwm =='False')] = 0      #Vectorisation for zero values
-y[(pwm =='True')] = (y-pwm) # #Vectorisation for sine wave
-plt.plot(t,y)
+    plt.subplot(3, 1, 1)
+    plt.ticklabel_format(axis='x', style='sci', scilimits=(0,1), useMathText=True)
+    plt.plot(time, sin1)
 
-plt.ylim([-3,3])
-plt.grid()
-plt.show()
+    plt.subplot(3, 1, 2)
+    plt.ticklabel_format(axis='x', style='sci', scilimits=(0, 1), useMathText=True)
+    plt.plot(time, pulse)
+
+    plt.subplot(3, 1, 3)
+    plt.ticklabel_format(axis='x', style='sci', scilimits=(0, 1), useMathText=True)
+    plt.plot(time, mod)
+
+    plt.show()
